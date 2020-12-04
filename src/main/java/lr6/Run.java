90dbece67e3ecabbba516a0abca016419dbb3f1e
Kudriashov <kudriashov>
2020-12-04 18:32:00 +0200

    updated files

--- src/main/java/lr6/Run.java
+++ src/main/java/lr6/Run.java
@@ -7,8 +7,8 @@ public class Run {
         Promise promise = new Promise();
         try {
             promise.add("Vlad", "Troll", "Boll", "1933-04-28");
-            promise.add("Dennis", "MacAlistair", "Ritchie", "1777-02-09");
-            promise.add("Kenneth", "Lane", "Thompson", "2001-03-04");
+            promise.add("Mir", "Tris", "Vis", "1777-02-09");
+            promise.add("Jor", "For", "Nic", "2001-03-04");
             promise.search("2001-03-04");
         } finally {
             promise.getConn().close();
